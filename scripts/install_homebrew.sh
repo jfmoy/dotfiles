f07a4d66c9ec08324a5e7e6a06168f08d8ec531f
Jean-François MOY <jean-francois.moy@osx-LGQQ2Y9RW9.local>
2022-05-05 16:36:29 +0100

    Huge refactor of bootstrapping scripts for macos
    - The scripts are compatible with M1 arm architecture and new paths.
    - Setup 1password-cli and dependencies automatically

--- scripts/install_homebrew.sh
+++ scripts/install_homebrew.sh
@@ -6,10 +6,15 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 . "$DIR/ansi"
 
 setupBrew() {
-	. "$DIR/macos_utils.sh"
+	. "$DIR/install_xcode.sh"
 	setupCommandLineTools
-	isavailable brew || curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
-	isavailable chezmoi || brew install chezmoi
+
+	if isavailable brew; then
+        ansi --blue "Brew already installed"
+    else
+		ansi --green "Brew is not installed, proceeding..."
+        curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+    fi
 }
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
